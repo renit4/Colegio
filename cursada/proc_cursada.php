f4997b88a4dc454c60dc81bb65879b40da465dc9
renit4 <145850245+renit4@users.noreply.github.com>
2024-05-24 19:00:52 -0300

    cambios

--- cursada/proc_cursada.php
+++ cursada/proc_cursada.php
@@ -1,7 +1,23 @@
 <?php
+    include("../conexion.php");
 
+    $alum= $_POST["alumnos"];
+    $mat = $_POST["materias"];
+    $num1  = $_POST["n1"];
+    $num2  = $_POST["n2"];
+    $asis  = $_POST["asistencia"];
+    $est  = $_POST["estado"];
 
+    $sql = "INSERT INTO `cursada`( `nota1`, `nota2`, `asistencia`, `estado`) 
+            VALUES ($num1,$num2,$asis,'$est')";
 
+    $res = mysqli_query($con,$sql);
+    if($res==true){
+        echo "excelente";
+    }
+    else{
+        echo "salio mal";
+    }
 
 
 ?>
